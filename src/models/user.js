948901bfb186d97bd9bedeb9b1931b017b579d9d
recep-demir <demir.rp@gmail.com>
2025-04-29 19:37:26 +0200

    feat: define User schema with validation and encryption

--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,35 @@
 const { mongoose } = require("../configs/dbConnection");
 const passwordEncrypt = require("../helpers/passwordEncrypt");
 
-const UserSchema = new mongoose.Schema({}, {});
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true    
+    },
+    password:{
+        type:String,
+        trim:true,
+        required:true,
+        set:(password)=>passwordEncrypt(password)
+    },
+    email:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true,
+        validate:[ (email)=>/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email) ,"Please enter a valid email address" ]
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    }
+
+},{collection:"Users",timestamps:true})
 
 module.exports = mongoose.model("User", UserSchema);
