f2e76b96bb71e810f27f2cb2e21f7ccc05019e8b
recep-demir <demir.rp@gmail.com>
2025-04-29 21:02:31 +0200

    feat: implement CRUD operations for Reservation with Swagger documentation

--- src/controllers/reservation.js
+++ src/controllers/reservation.js
@@ -2,4 +2,78 @@
 
 const Reservation = require("../models/reservation");
 
-module.exports = {};
+module.exports = {
+  list: async (req, res) => {
+            /* 
+        #swagger.tags = ['Rezervations']
+        #swagger.summary = 'List Rezervations'
+        #swagger.desription = `
+            You can send query with endpoint for filter[], search[], sort[], page and limit.
+            <ul> Examples usage:
+                <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                <li>URL/?<b>page=2&limit=1</b></li>
+            </ul>
+        `
+    */
+    const result = await res.getModelList(Reservation);
+    res.status(200).send({
+      error: false,
+      details: await res.getModelListDetails(Reservation),
+      result,
+    });
+  },
+
+  create: async (req, res) => {
+            /* 
+                #swagger.tags = ['Rezervations']
+                #swagger.summary = 'Create User'
+            */
+    const result = await Reservation.create(req.body);
+    res.status(201).send({
+      error: false,
+      result,
+    });
+  },
+
+  read: async (req, res) => {
+            /* 
+                #swagger.tags = ['Rezervations']
+                #swagger.summary = 'Get Single User'
+            */
+    const result = await Reservation.findById(req.params.id);
+    res.status(200).send({
+      error: false,
+      result,
+    });
+  },
+
+  update: async (req, res) => {
+            /* 
+           #swagger.tags = ['Rezervations']
+           #swagger.summary = 'Update User'
+       */
+    const result = await Reservation.findByIdAndUpdate(req.params.id, req.body, {
+      runValidators: true,
+      new: true,
+    });
+    res.status(202).send({
+      error: false,
+      result,
+    });
+  },
+
+  delete: async (req, res) => {
+        /* 
+        #swagger.tags = ['Rezervations']
+        #swagger.summary = 'Delete User'
+    */
+    const result = await Reservation.findByIdAndDelete(req.params.id);
+    if (result) return res.status(204).end();
+    res.status(404).send({
+      error: true,
+      message: "Reservation not found or already deleted",
+    });
+  },
+};
--- src/models/reservation.js
+++ src/models/reservation.js
@@ -2,6 +2,43 @@
 
 const { mongoose } = require("../configs/dbConnection");
 
-const ReservationSchema = new mongoose.Schema({}, {});
+const ReservationSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  roomId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Room", // Room modeli varsa
+    required: true,
+  },
+  arrival_date: {
+    type: Date,
+    required: true,
+    default:Date.now
+  },
+  departure_date: {
+    type: Date,
+    required: true,
+    
+  },
+  guest_number: {
+    type: Number,
+    required: true,
+  },
+  night: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  totalprice: {
+    type: Number,
+    required: true,
+  },
+}, { collection: "Reservations", timestamps: true });
 
 module.exports = mongoose.model("Reservation", ReservationSchema);
