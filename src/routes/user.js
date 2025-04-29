3ffb138583fd21d376ce66e71ee8f55ca56ad2e4
recep-demir <demir.rp@gmail.com>
2025-04-29 20:14:48 +0200

    fix: correct user route definitions and ensure proper controller imports

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,14 @@
 "use strict";
 
-const router = require("express").Router();
-const {} = require("../controllers/user");
+const router = require('express').Router()
+const user=require("../controllers/user")
+
+router.route("/").get(user.list).post(user.create)
+
+router.route("/:id")
+.get(user.read)
+.put(user.update)
+.patch(user.update)
+.delete(user.deleteUser)
 
 module.exports = router;
