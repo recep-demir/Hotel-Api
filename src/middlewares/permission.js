6a39953288315185b9cddf803a4411f9787d6018
recep-demir <demir.rp@gmail.com>
2025-04-29 19:24:35 +0200

    feat: add permission middleware for user login validation

---
+++ src/middlewares/permission.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+  isLogin: (req, res, next) => {
+    if (req.user && req.user.isActive) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("No Permission: You Must Login");
+    }
+  },
+};
